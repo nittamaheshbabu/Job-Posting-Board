520e41ea4ddae0ca8185b154184efd1b917307dd
Nitta Mahesh Babu <126187053+nittamaheshbabu@users.noreply.github.com>
2024-10-20 20:22:32 +0530

    Delete old backend directory

--- backend/Routes/signUpRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { companyRegistration, companyVerification } = require('../Controller/signUpController');
-
-const router = express.Router();
-
-router.post("/company-registration", companyRegistration);
-router.post("/company-verification", companyVerification);
-
-module.exports = router;
